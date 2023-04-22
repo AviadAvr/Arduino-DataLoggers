3438cad934b2f1805b4191b40b0cf95e75887b38
Aviad Avraham <124437932+AviadAvr@users.noreply.github.com>
2023-04-22 16:45:03 +0200

    Update example.py

--- example.py
+++ example.py
@@ -7,8 +7,15 @@ df['Date & Time'] = pd.to_datetime(df['Date & Time'], dayfirst=True)
 df = df.set_index('Date & Time')
 pressure = pd.DataFrame()
 pressure['pressure'] = df[' Pressure (mBar)']
-pressure['pressure_mean'] = pressure['pressure'].resample('0.5T').mean() # measurements are taken every ~seconds, the resampling takes the mean of every 30 seconds (~6 data points) to average out measurement issues.
-pressure['pressure_med'] = pressure['pressure_mean'].resample('6T').median() # further smoothing using median is important to remove negative values. i found that downsampling to every 6-8 minutes gave the smoothest trends, while staying true to the original data's form.
+
+# Here i use pd.rolling() to calculate the rolling median, while calculating the moving average first to smooth out the data a bit.
+pressure['pressure_mean'] = pressure['pressure'].rolling(window='1T').mean()
+pressure['pressure_med'] = pressure['pressure_mean'].rolling(window='10T').median()
+
+# alternatively, you can use resampling in order to smooth out the data a bit differently. I'm not sure which is the more 'correct' way to do it, from a statistics perspective, but the resulting trend isn't as smooth as with the moving average/median method.
+# pressure['pressure_mean'] = pressure['pressure'].resample('0.5T').mean() # measurements are taken every ~seconds, the resampling takes the mean of every 30 seconds (~6 data points) to average out measurement issues.
+# pressure['pressure_med'] = pressure['pressure_mean'].resample('6T').median() # further smoothing using median is important to remove negative values. i found that downsampling to every 6-8 minutes gave the smoothest trends, while staying true to the original data's form.
+
 pressure.pressure_med.dropna().plot()
 pressure.pressure.plot(alpha=0.3) # Just to compare the smoothed trend to the original data.
 plt.legend()
